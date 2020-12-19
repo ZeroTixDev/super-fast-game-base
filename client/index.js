3e378156f603dc4ce9ef0845824c1845e3fa0a20
ZeroTix <weddnremy@gmail.com>
2020-12-18 20:47:22 -0500

    testing to see if server update

--- client/index.js
+++ client/index.js
@@ -94,7 +94,7 @@ function join() {
    attempt();
    const interval = setInterval(attempt, 2000);
 }
-console.log('correction... v7');
+console.log('correction... v8');
 function recon(data, player) {
    const lastProcessEncoded = encode('lastProcessedTick');
    const posEncoded = encode('pos');
