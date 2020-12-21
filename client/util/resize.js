0fa70873ee5b90d0cb360344d9451921ac21d0d3
ZeroTix <weddnremy@gmail.com>
2020-12-21 12:06:31 -0500

    new resiziing

--- client/util/resize.js
+++ client/util/resize.js
@@ -8,5 +8,4 @@ module.exports = function resize(canvas) {
    canvas.style.transform = 'scale(' + scale + ')';
    canvas.style.left = (winw - canvas.width) / 2 + 'px';
    canvas.style.top = (winh - canvas.height) / 2 + 'px';
-   return scale;
 };
