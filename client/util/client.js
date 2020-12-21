f464cd29b5aa6f2ae0d5814fa165c18787706366
ZeroTix <weddnremy@gmail.com>
2020-12-21 15:41:33 -0500

    Revert "death spiral fix & lava sync"
    This reverts commit e2b9a1e67f5120e0a2aa9b467db58d7548b59b2e.

--- client/util/client.js
+++ client/util/client.js
@@ -284,9 +284,6 @@ module.exports = class Client {
             this.lava.up = msg[lavaEncoded].up;
             this.lava.down = msg[lavaEncoded].down;
             this.isJoined = true;
-            for (let i = 0; i < this.tick; i++) {
-               this.lava.simulate();
-            }
          }
          const initPackEncoded = encode('initPack');
          if (msg[initPackEncoded]) {
