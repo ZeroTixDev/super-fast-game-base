f9d3ff21f21303edd7bbc8c9975565a6a242d9a2
ZeroTix <weddnremy@gmail.com>
2020-12-22 14:00:09 -0500

    revert dont look in ball.js

---
+++ server/util/ball.js
@@ -0,0 +1,132 @@
+'use strict';
+
+const Vector = require('./vector');
+const { simulatePlayer } = require('../.././shared/simulate');
+const { encode } = require('../.././shared/name');
+
+module.exports = class Ball {
+   constructor(id, pos) {
+      this.id = id;
+      this.pos = pos;
+      this.vel = new Vector(0, 0);
+      this.maxSpd = (500 * 1) / 60;
+      this.radius = 30;
+      this.sendingPos = this.pos.copy();
+      this.friction = 0.82;
+   }
+   static getAllInitPack(players) {
+      const initPacks = [];
+      for (const i in players) {
+         initPacks.push(players[i].getInitPack());
+      }
+      return initPacks;
+   }
+   static updatePlayers({ players, arena }) {
+      const oldState = { ...players };
+      for (const i of Object.keys(players)) {
+         players[i].update(arena, oldState);
+      }
+   }
+   static pack(players) {
+      const pack = [];
+      for (const i in players) {
+         const object = players[i].getUpdatePack();
+         if (Object.keys(object).length > 0) pack.push(object);
+      }
+      return pack;
+   }
+   getUpdatePack() {
+      const object = Object.create(null);
+      if (!this.pos.equal(this.sendingPos)) {
+         object[encode('pos')] = { x: this.pos.round().x, y: this.pos.round().y };
+         object[encode('lastProcessedTick')] = this.lastProcessedTick;
+         this.sendingPos = this.pos.copy();
+      }
+      if (this.chatTime > 0) {
+         object[encode('chatTime')] = Math.round(this.chatTime * 100) / 100;
+         if (this.chatMsg.length > 0) {
+            object[encode('chatMsg')] = this.chatMsg;
+         }
+      }
+      if (Object.keys(object).length > 0) {
+         object[encode('id')] = this.id;
+      }
+      return object;
+   }
+   static collision({ playerArray, players }) {
+      for (let i = 0; i < playerArray.length; i++) {
+         for (let j = i + 1; j < playerArray.length; j++) {
+            const player1 = players[playerArray[i][0]];
+            const player2 = players[playerArray[j][0]];
+            if (
+               Math.sqrt(
+                  Math.abs(Math.pow(player2.pos.x - player1.pos.x, 2) + Math.pow(player2.pos.y - player1.pos.y, 2))
+               ) < 60
+            ) {
+               const distance = Math.sqrt(
+                  Math.abs(Math.pow(player2.pos.x - player1.pos.x, 2) + Math.pow(player2.pos.y - player1.pos.y, 2))
+               );
+               const rotate = Math.atan2(player2.pos.y - player1.pos.y, player2.pos.x - player1.pos.x);
+               player2.pos.x += ((Math.cos(rotate) * 1) / distance) * 500;
+               player1.pos.x -= ((Math.cos(rotate) * 1) / distance) * 500;
+               player2.pos.y += ((Math.sin(rotate) * 1) / distance) * 500;
+               player1.pos.y -= ((Math.sin(rotate) * 1) / distance) * 500;
+            }
+         }
+      }
+   }
+   getInitPack() {
+      const object = Object.create(null);
+      object[encode('radius')] = this.radius;
+      object[encode('maxSpd')] = this.maxSpd;
+      object[encode('chatTime')] = Math.round(this.chatTime * 100) / 100;
+      object[encode('chatMsg')] = this.chatMsg;
+      object[encode('pos')] = { x: this.pos.round().x, y: this.pos.round().y };
+      object[encode('id')] = this.id;
+      return object;
+   }
+   update(arena, players) {
+      if (this.pendingInputs.length === 0) {
+         simulatePlayer({ players, id: this.id, arena }, { up: false, down: false, left: false, right: false });
+      } else {
+         while (this.pendingInputs.length >= 1) {
+            simulatePlayer({ players, id: this.id, arena }, this.pendingInputs[0].input);
+            this.lastProcessedTick = this.pendingInputs[0].tick;
+            this.pendingInputs.shift();
+         }
+      }
+   }
+};
+
+/*const randomConso = function () {
+   const index = Math.round(Math.random() * 20);
+   const consos = [
+      'b',
+      'c',
+      'd',
+      'f',
+      'g',
+      'h',
+      'j',
+      'k',
+      'l',
+      'm',
+      'n',
+      'p',
+      'q',
+      'r',
+      's',
+      't',
+      'v',
+      'w',
+      'x',
+      'y',
+      'z',
+   ];
+   return consos[index];
+};
+const randomVowel = function () {
+   const index = Math.round(Math.random() * 4);
+   const vowels = ['a', 'e', 'i', 'o', 'u'];
+   return vowels[index];
+};*/
